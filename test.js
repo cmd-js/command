ed68a17ea12350ba1472ae660faa16fb8c10205c
Scott Corgan <scottcorgan@gmail.com>
2015-09-19 10:59:17 -0700

    update test for alias and handler

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 var test = require('tape')
 var command = require('./')
-var name = require('cmd-name')
-var use = require('cmd-use')
+var alias = require('cmd-alias')
+var handler = require('cmd-handler')
 
 test('command()', function (t) {
 
   var fn1 = function () {}
 
   var c = command(
-    name('test'),
-    use(fn1)
+    alias('test'),
+    handler(fn1)
   )
 
   t.equal(typeof c, 'function', 'returns a function')
@@ -18,7 +18,7 @@ test('command()', function (t) {
     type: 'command',
     value: {
       alias: ['test'],
-      'function': [fn1]
+      'handler': [fn1]
     },
     options: undefined
   }, 'basic execution')
@@ -27,7 +27,7 @@ test('command()', function (t) {
     type: 'command',
     value: {
       alias: ['test'],
-      'function': [fn1]
+      'handler': [fn1]
     },
     options: {key: 'value'}
   }, 'with options')
@@ -40,17 +40,17 @@ test('combines multiple values', function (t) {
   var fn1 = function () {}
   var fn2 = function () {}
   var c = command(
-    name('one'),
-    name('two'),
-    use(fn1),
-    use(fn2)
+    alias('one'),
+    alias('two'),
+    handler(fn1),
+    handler(fn2)
   )
 
   t.deepEqual(c(), {
     type: 'command',
     value: {
       alias: ['one', 'two'],
-      'function': [fn1, fn2]
+      'handler': [fn1, fn2]
     },
     options: undefined
   }, 'values')
